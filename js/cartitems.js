d2b092f810895783986e7535ce01d3ba4503ad23
Andharikar9011 <48117912+Andharikar9011@users.noreply.github.com>
2020-10-05 18:43:48 +0530

    done

--- js/cartitems.js
+++ js/cartitems.js
@@ -245,14 +245,14 @@ var mass = sessionStorage.getItem("per").split(",");
 var prodlist = sessionStorage.getItem("products").split(",");
 var quantitylist = sessionStorage.getItem("quantity").split(",");
 var totalvalue=0;
-var message = "Welcome This is Quick N Fresh !!! \nYour Cart Contains: \n";
+var message = "Welcome This is Quick N Fresh !!! %0D%0A Your Cart Contains: %0D%0A";
 for (let index = 0; index < prodlist.length; index++) {
   totalvalue = totalvalue + parseInt(val[index])*parseInt(quantitylist[index])      
 }
 for (let ind = 0; ind < prodlist.length; ind++) {
-  message =message + "Item "+(ind+1)+" : "+prodlist[ind]+" Price: ₹"+val[ind]+" / "+mass[ind]+" Quantity : "+quantitylist[ind]+" "+mass[ind]+"\n";
+  message =message + "Item "+(ind+1)+" : "+prodlist[ind]+" Price: ₹"+val[ind]+" / "+mass[ind]+" Quantity : "+quantitylist[ind]+" "+mass[ind]+"%0D%0A";
 }
-message = message + "Total Items Cost = ₹"+totalvalue +"\nKindly Share your Location so we can reach you Quick N Fresh !!!"
+message = message + "Total Items Cost = ₹"+totalvalue +"%0D%0AKindly Share your Location so we can reach you Quick N Fresh !!!"
   var win = window.open(`https://wa.me/918983778453?text=${message}`, '_blank');
   // alert(message);
   // alert("Order Sent To Distributer.")
